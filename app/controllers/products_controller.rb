ae140f13addecedabc165dc775bf9a9572465832
Andrey Yurov <a.yurov6@gmail.com>
2025-08-17 06:56:30 +0400

    Add create action for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,18 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def product_params
+    params.expect(product: [ :name ])
+  end
 end
