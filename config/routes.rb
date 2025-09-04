c3fcc6d2431426015bc3cc1445eaa4728f82a724
Andrey Yurov <a.yurov6@gmail.com>
2025-09-04 17:58:36 +0400

    Add subscribers to Products

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
