79545bd13bcdfec432edc3da1aeb87cbc391032f
Andrey Yurov <a.yurov6@gmail.com>
2025-09-05 15:32:25 +0400

    Rubocop fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,8 @@
 class Product < ApplicationRecord
-
   include Notifications
 
   has_one_attached :featured_image
   has_rich_text :description
   validates :name, presence: true
   validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
-
 end
